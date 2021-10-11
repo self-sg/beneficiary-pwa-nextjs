5f839fe70cc6522830dc8703a48c484d433b0868
Myyk Seok <myyk.seok@gmail.com>
2021-10-11 14:59:55 +0000

    add bottom nav
    Not linked yet or even the correct categories

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import BottomNav from './bottomnavbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -43,17 +44,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <BottomNav name='home' />
     </div>
   )
 }
