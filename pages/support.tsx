89f7f59c2810e56c71e756b1163d2a1d3b2bb0df
by-bai <baihe.soh@gmail.com>
2021-11-20 19:22:45 +0800

    add top nav with back button functionality

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,10 +1,12 @@
 import BottomNav from '../components/BottomNav'
 import styles from '../styles/Home.module.css'
+import TopNav from '../components/TopNav'
 
 export default function Support() {
   return (
     <div className={styles.container}>
       {}
+      <TopNav pageName={'Support'} displayBackButton={true} />
       Support
       <BottomNav name="support" />
     </div>
