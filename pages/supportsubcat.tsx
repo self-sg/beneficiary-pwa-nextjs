75fab4e497d49aa5ad923f46b2210e5d509cd2c8
by-bai <baihe.soh@gmail.com>
2021-12-09 01:03:13 +0800

    add sub category component & render all subcats

--- pages/supportsubcat.tsx
+++ pages/supportsubcat.tsx
@@ -1,8 +1,10 @@
 import BottomNav from '../components/BottomNav'
 import styles from '../styles/Home.module.css'
 import TopNav from '../components/TopNav'
-import Button from '../components/Button'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+import career from '../public/assets/category/career.svg'
+import SubCategory from '../components/SubCategory'
 
 export default function Support() {
   const router = useRouter()
@@ -13,7 +15,29 @@ export default function Support() {
     <div className={styles.container}>
       {}
       <TopNav pageName={'Support Subcategory'} displayBackButton={true} />
-      <Button type="primary" text="View listing" clickHandler={goNextPage} />
+      {/* <Button type="primary" text="View listing" clickHandler={goNextPage} /> */}
+      <Image src={career} />
+      <h4>Career</h4> {/*  TODO: change weight to semibold */}
+      <SubCategory style={'career01'} text={'Work Programme'} />
+      <SubCategory style={'career02'} text={'Career Guidance & Coaching'} />
+      <SubCategory style={'healthcare01'} text={'Mental'} />
+      <SubCategory style={'healthcare02'} text={'Physical'} />
+      <SubCategory style={'healthcare03'} text={'Special Needs'} />
+      <SubCategory style={'financial01'} text={'Education'} />
+      <SubCategory style={'financial02'} text={'Food'} />
+      <SubCategory style={'financial03'} text={'Healthcare'} />
+      <SubCategory style={'financial04'} text={'COVID'} />
+      <SubCategory style={'self01'} text={'Upskilling Course'} />
+      <SubCategory style={'self02'} text={'Language Class'} />
+      <SubCategory style={'family01'} text={'Marriage'} />
+      <SubCategory style={'family02'} text={'Parenting'} />
+      <SubCategory style={'children01'} text={'Education Class'} />
+      <SubCategory style={'children02'} text={'Childcare'} />
+      <SubCategory style={'children03'} text={'Enrichment Class'} />
+      <SubCategory style={'children04'} text={'School Searching'} />
+      <SubCategory style={'legal01'} text={'Free Legal Clinic'} />
+      <SubCategory style={'legal02'} text={'Child Rights'} />
+      <SubCategory style={'legal03'} text={'Employment Rights'} />
       <BottomNav name="supportsubcat" />
     </div>
   )
