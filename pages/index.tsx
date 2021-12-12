e92ff87950ec736213e13634ff1c26bfe57f5a39
by-bai <baihe.soh@gmail.com>
2021-12-12 22:08:51 +0800

    fix featureName prop in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Home</h1>
-      <BottomNav name="home" />
+      <BottomNav featureName="home" />
     </div>
   )
 }
