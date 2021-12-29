1651416782cf0e3c3ab2c68941b85cbb7915c02f
bai <baihe.soh@gmail.com>
2021-12-30 07:34:42 +0800

    create reusable Support Card component & complete UI for Support Listing page  (#29)
    * add dynamic routing to support listing page
    
    * remove unnecessary console logs
    
    * start on support card component
    
    * add subcategory header on support listing page
    
    * add organisation icon to assets
    
    * complete UI for support card component
    
    * update states in support card component
    
    * update css for support card component

--- pages/support/[category]/[subcategory].tsx
+++ pages/support/[category]/[subcategory].tsx
@@ -1,21 +1,59 @@
 import BottomNav from '../../../components/BottomNav'
-import styles from '../../../styles/Page.module.css'
+import pageStyles from '../../../styles/Page.module.css'
+import styles from '../../../styles/SupportListing.module.css'
 import TopNav from '../../../components/TopNav'
+import Card from '../../../components/support/Card'
 import { useRouter } from 'next/router'
 
 export default function SupportListing() {
+  const support_listing = [
+    {
+      title:
+        'Help families in need take a break from caregiving duties, 6-10pm every weeknight',
+      organisation: 'Morning Star Community Services',
+      region: 'Central, West, East',
+      type: 'Childcare'
+    },
+    {
+      title:
+        'More financial assistance for Childcare fees on top of basic and additional subsidies',
+      organisation: 'Ministry of Social Family & Development (MSF)',
+      region: 'Central, West, East, North, South',
+      type: 'Childcare'
+    },
+    {
+      title:
+        'Help families in need take a break from caregiving duties, 6-10pm every weeknight',
+      organisation: 'Morning Star Community Services',
+      region: 'Central, West, East',
+      type: 'Childcare'
+    }
+  ]
   const router = useRouter()
   let category = router.asPath.split('/')[2]
   category = category.charAt(0).toUpperCase() + category.slice(1)
+  let subcategory = router.asPath.split('/')[3].replace('-', ' ')
+  const index = subcategory.search('-')
+  if (index !== -1) {
+    // handles Career Guidance & Coaching
+    subcategory =
+      subcategory.substring(0, index) + ' & ' + subcategory.substring(index + 1)
+  }
 
   return (
-    <div className={styles.container}>
-      {}
+    <div className={pageStyles.container24}>
       <TopNav
         pageName={category}
         displayBackButton={true}
         displayFilter={true}
       />
+      <div className={styles.alignment}>
+        <span className="text-body-main">{subcategory}</span>
+      </div>
+      {support_listing.map((support_item) => (
+        <Card data={support_item} />
+      ))}
+
       <BottomNav featureName="support" />
     </div>
   )
